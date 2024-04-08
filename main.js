871194773815301cf24a0ae9306a5765bfd19447
Yuan Huang@MBA <huangjenyuan@gmail.com>
2024-04-08 21:18:07 +0800

    remove WOW

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const header = document.querySelector('.header');
-const wow = document.querySelector('.wow');
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('open');
   header.classList.toggle('open-menu');
 });
-
-setTimeout(() => {
-  wow.style.opacity = 1;
-}, 800);
